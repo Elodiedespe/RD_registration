6808fd2f32c57e9ae923e9676a91a911a305d624
Elodie DOGERDESPEVILLE <edogerde@is220362.intra.cea.fr>
2015-11-27 19:23:09 +0100

    change legend from gray to u.a

--- variabilite-InterIntra_rd.py
+++ variabilite-InterIntra_rd.py
@@ -3,7 +3,7 @@ import numpy as np
 import seaborn as sns
 import pandas as pd
 
-df = pd.read_csv('/home/edogerde/Desktop/variation_interIntra_RD.csv') 
+df = pd.read_csv('/media/edogerde/MY PASSPORT/for_git/RD_registration/variation_interIntra_RD.csv') 
 plt.plot(df.Results[df.Dose=='Dose 1'])
 plt.plot(df.Results[df.Dose=='Dose 2'])
 plt.plot(df.Results[df.Dose=='Dose 3'])
@@ -13,4 +13,4 @@ plt.xlim(0, 3)
 plt.ylim(20, 750)
 plt.xlabel("Dose en fonction de plusieurs points dans le cerveau")
 plt.ylabel("Dose en u.a") 
-
+plt.show()
